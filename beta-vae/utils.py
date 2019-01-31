423500c52d8ef281495c7093a91166250af8175b
gtegner <gustaf.tegner@gmail.com>
2019-02-01 00:59:25 +0100

    #

--- beta-vae/utils.py
+++ beta-vae/utils.py
@@ -1,3 +1,3 @@
 import torch
 def KL_loss(mu, log_sigma):
-    return 0.5 * torch.mean(1 - 2 * log_sigma + mu*mu + torch.exp(2 * log_sigma))
+    return -0.5 * torch.mean(1 + log_sigma - mu*mu - torch.exp(2 * log_sigma))
